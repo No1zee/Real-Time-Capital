c817a750738ada802104e9c7ef8a3ae8ac13228d
Noizee <edwitter123magejo@gmail.com>
2025-12-08 10:20:59 +0200

    Set default theme to corporate

--- pawn-system/src/app/providers.tsx
+++ pawn-system/src/app/providers.tsx
@@ -6,7 +6,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <ThemeProvider
             attribute="data-theme"
-            defaultTheme="luxury"
+            defaultTheme="corporate"
             enableSystem={false}
             themes={["luxury", "corporate", "modern"]}
         >
